9d0fbcee7621ccdab1d6bbb22799255ccae0a7dc
James Dolan <jwdolan820@gmail.com>
2018-01-23 22:44:16 +0000

    Added login routes to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,23 @@ class UsersController < ApplicationController
      end
    end
 
+   get '/login' do
+     @error_message = params[:error]
+     if !session[:user_id]
+       erb :'users/login'
+     else
+       redirect '/tracks'
+     end
+   end
+
+   post '/login' do
+     user = User.find_by(:username => params[:username])
+     if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect "/tracks"
+     else
+       redirect to '/signup'
+     end
+   end
+
 end
